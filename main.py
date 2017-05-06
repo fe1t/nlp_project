04274061f80bb67ed44d6f3cb948bb32e1c45f6b
fe1t <nuttapon.t@ku.th>
2017-05-06 16:04:31 +0700

    [+] edit main.py almost complete

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # - * - coding: utf-8 - * -
+
 # 1COLUMNS : isCommonDict(w-0), isDictName(w_0), isPrefixOrg(w_0), isPrefixPer(w_0), isInPrefixOrg(w_-7 - w_-2), CLASS
 # 2COLUMNS : isCommonDict(w-0), isDictName(w_0), isOrgName(w_0), isPrefixOrg(w_0), isPrefixPer(w_0), isInPrefixOrg(w_-7 - w_-2), CLASS
-# CLASS : {per, per_start, per_cont, per_end, org, org_start, org_end}
+# CLASS : {per, per_start, per_cont, per_end, org, org_start, org_cont, org_end}
 
-import glob, sys
+import glob, sys, re
 
 def convert_to_utf(filenames):
     for _file in filenames:
@@ -13,18 +14,54 @@ def convert_to_utf(filenames):
         with open(_file, "w") as f:
             f.write(data.encode('utf8'))
 
+def read_format(f):
+    return [ _data.rstrip() for _data in f.readlines() ]
+
+
 taboo = ["org", "per", "loc"]
 person_files = glob.glob("./dictionary/person/*")
 org_files = glob.glob("./dictionary/org/*")
 common_files = glob.glob("./dictionary/common/*")
 # convert_to_utf(common_files)
 
-with open(train, "r") as f:
+with open("train/98JUL5_2.txt", "r") as f:
     data = filter(lambda x: x != "\r\n" and x != " ", f.read().split('|'))
 
-"""
-for i in data[10:100]:
-    if i == "ว่า":
-        print i
-"""
+learner = list()
+common_dict = common_files[2]
+name_dict = person_files[6]
+prefix_org = org_files[15]
+prefix_person = person_files[-1]
+w27_prefix_org = 0
+expr = re.compile(r'(.*?)\((per_start|per_cont|per_end|org_start|org_cont|org_end|per|org)\)')
 
+with open(common_dict, 'r') as f:
+    common_dict = read_format(f)
+with open(name_dict, 'r') as f:
+    name_dict = read_format(f)
+with open(prefix_org, 'r') as f:
+    prefix_org = read_format(f)
+with open(prefix_person, 'r') as f:
+    prefix_person = read_format(f)
+
+for _data in data[10:100]:
+    row = list()
+    row.append(_data in common_dict)
+    row.append(_data in name_dict)
+    check_org = _data in prefix_org
+    w27_prefix_org = 7 if check_org else w27_prefix_org - 1
+    row.append(check_org)
+    row.append(_data in prefix_person)
+    row.append(True if w27_prefix_org > 1 else False)
+    matched = expr.match(_data)
+    if matched:
+        word = matched.group(1)
+        _class = matched.group(2)
+        row.append(_class.upper())
+    else:
+        row.append("OTHER")
+    learner.append(row)
+    if w27_prefix_org < 0:
+        w27_prefix_org = 0
+
+print learner
